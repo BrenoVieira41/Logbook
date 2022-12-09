4ea5d9dd29550022f353d8d8210d8c890f095c85
Breno Vieira <brenohvl@hotmail.com>
2022-12-08 22:50:40 -0300

    feat: add string

---
+++ JS/string/index.js
@@ -0,0 +1,9 @@
+const var1 = 'válido';
+const var2 = "válido";
+const var3 = `igualmente ${var1}`;
+const var4 = 'igualmente ' + var1;
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log(var4);
